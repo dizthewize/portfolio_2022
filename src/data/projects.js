c4636d69f1e8eb98b5eb2cb06c2cf459b1ef7b76
Martez Howard <conner.martez@gmail.com>
2022-04-24 18:54:40 -0700

    initial commit

---
+++ src/data/projects.js
@@ -0,0 +1,61 @@
+import IMG1 from '../assets/ubisoft.png'
+import IMG2 from '../assets/makena.png'
+import IMG3 from '../assets/endpointclosing.png'
+import IMG4 from '../assets/banklesshq.png'
+import IMG5 from '../assets/winstonretail.png'
+import IMG6 from '../assets/allstateds.png'
+import IMG7 from '../assets/digital_innerprize.png'
+import IMG8 from '../assets/bigcommerce_zendesk.png'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Ubisoft',
+    demo: 'https://www.ubisoft.com/en-us/'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Makena Capital Management',
+    demo: 'https://investments.makenacap.com/login'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Endpoint Closing',
+    demo: 'https://www.endpointclosing.com/'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Bankless HQ',
+    demo: 'https://gobankless.ghost.io/'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Wintell',
+    demo: 'http://client-stage.wrs.presencepg.com/login'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Allstate DS',
+    demo: 'https://www.allstate.com/anon/commoncostlyclaims/default.aspx'
+  },
+  {
+    id: 7,
+    image: IMG7,
+    title: 'Digital InnerPrize',
+    demo: 'https://compassionate-chandrasekhar-ad5c41.netlify.app/'
+  },
+  {
+    id: 8,
+    image: IMG8,
+    title: 'BigCommerce Zendesk App',
+    demo: 'https://www.zendesk.com/'
+  }
+]
+
+export { data }
