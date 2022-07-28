6e380250a71cf8e7e1b8a89fab4b8b483bef46d6
Martez Howard <conner.martez@gmail.com>
2022-07-27 19:48:56 -0700

    update header text

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Martez Howard</h1>
-        <h5 className="text-light">Full Stack / DevOps Engineer</h5>
+        <h5 className="text-light">DevOps Engineer</h5>
         <CTA />
         <HeaderSocial />
 
